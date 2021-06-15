72a9d0d10047a33e4090977941fa82516f456f9c
Mateo Gabriel Rios <mateo903@gmail.com>
2021-06-15 15:02:28 -0300

    Initializing project

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+const app = express();
+
+const port = process.env.PORT || 3000
+
+app.get("/", (req, res) => {
+  res.send("Hola Mundo")
+});
+
+
+app.listen(port, () => {
+  console.log("sevidor en el puerto ", port)
+});
