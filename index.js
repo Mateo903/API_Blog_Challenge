b8f577713b19a71168b20dbb2338d312c2dc6f7e
Mateo Gabriel Rios <mateo903@gmail.com>
2021-06-16 14:19:06 -0300

    Setting the Express server

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express');
-
 const app = express();
-
 const port = process.env.PORT || 3000
+
+
+app.use(express.urlencoded());
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Hola Mundo")
