e681dd40a9f54cc68233fe92045817e3a3d1ee47
Mateo Gabriel Rios <mateo903@gmail.com>
2021-06-16 16:26:24 -0300

    Database Post Created

--- db.js
+++ db.js
@@ -1,6 +1,19 @@
 const Sequelize = require("sequelize");
 
+const PostModel = require("./models/Posts");
+
 const sequelize = new Sequelize("2HPGLpTSyN", "2HPGLpTSyN", "Q3RYcAAsRe", {
   host: 'remotemysql.com',
   dialect: "mysql"
 });
+
+const Post = PostModel(sequelize, Sequelize);
+
+sequelize.sync({force: false})
+  .then(() => {
+    console.log("Sync success")
+  });
+
+  module.exports = {
+    Post
+  }
--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000
 
+require("./db")
 
 app.use(express.urlencoded());
 app.use(express.json());
